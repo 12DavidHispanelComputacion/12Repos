9ebfefd767e4f10c44c17eeaa2f6c18e94c90535
12DavidHispanelComputacion <162484344+12DavidHispanelComputacion@users.noreply.github.com>
2024-03-11 10:50:37 -0600

    Update Tarea_ParOImpar.java

--- Tarea_ParOImpar/src/main/java/com/mycompany/tarea_paroimpar/Tarea_ParOImpar.java
+++ Tarea_ParOImpar/src/main/java/com/mycompany/tarea_paroimpar/Tarea_ParOImpar.java
@@ -15,12 +15,12 @@ public class Tarea_ParOImpar {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        System.out.println("Ingrese un numero: ");
-        int n = teclado.nextInt();
+        System.out.println("Ingrese un numero: "); //Esto es para que logremos ingresar cualquier digito media vez sea un numero entero-
+        int n = teclado.nextInt(); // Esto es para poder ingrear el valor.
         
-        if (n%2==0){
-            System.out.println("Es un numero par");
-        }else{
+        if (n%2==0){ //Esto es para poder diferenciar si es un numero par o impar.
+            System.out.println("Es un numero par"); // Esto da un mensaje diciendo que es un numero par.
+        }else{ // Esto nos dice que DE OTRO MODO el resultado sea impar.
             System.out.println("No es un numero par");
         }
         teclado.close();
